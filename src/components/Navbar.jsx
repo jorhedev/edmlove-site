ea924d8d58d1f0c03f4f7553df09c4b2b730f9f9
Jorge Tolentino <hijorhe@gmail.com>
2024-02-02 23:41:51 -0600

    FIX LOGO 1.1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Link } from 'react-router-dom'
-import {logo} from '../assets/LOGOB.png'
+import logo from '../assets/LOGOB.png'
 
 const navigation = [
   { name: 'Home', href: '/' },
